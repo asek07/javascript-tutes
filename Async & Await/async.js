af04807b2fe951128dddefb52965f479a558c5c6
Andy Sekulovski <andy.sek94@gmail.com>
2019-01-07 15:58:53 +1100

    async method added

---
+++ Async & Await/async.js
@@ -0,0 +1,35 @@
+//Async with promises
+
+//Promise function
+const sayHi = name => {
+  return new Promise((res, rej) => {
+    setTimeout(() => {  //happens after 1 second
+        res("Hello, " + name);
+    }, 1000)
+  })
+}
+
+const greetEveryone = async () => {
+  const a = await sayHi("Andy");
+  const b = await sayHi("Joe");
+  const c = await sayHi("Jim");
+
+  let vals = [];
+
+  vals.push(a);
+  vals.push(b);
+  vals.push(c);
+
+  return vals;
+}
+
+//Call greetEveryone method, WAIT until it's complete
+//use then() to get what greetEveryone() returned
+//do what you need with that data
+greetEveryone().then(names => {
+  //greetEveryone returns an array of names
+  //create a loop that renders each index
+  for(i=0; i < names.length; i++) {
+    console.log(names[i]);
+  }
+})
